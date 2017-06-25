51ed3e25c9f7388e67e99e335c04b2ab1f2e8d78
yuralala8 <yc1435@nyu.edu>
2017-06-24 21:28:00 -0400

    keys of hash

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,15 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
-  end
+    def keys_of(*arguments)
+        ary = Array.new
+        arguments.each do |v|
+            if self.values.include?(v)
+                self.each do |k, values|
+                    if values == v
+                        ary << k
+                    end
+                end
+            end
+        end
+        ary
+    end
 end
